3df5687117002ba967c033fd53c28c3c26e6eb04
Jhonathan Henrique <Jhonathan.Engineer@gmail.com>
2025-05-14 10:43:33 -0300

    Chore: add 2 functions - Extrair and transformar

--- exemplo_01.py
+++ exemplo_01.py
@@ -1,7 +1,25 @@
 import requests
 
-url = "https://api.coinbase.com/v2/prices/spot"
+def extrair():
+    '''Aqui criei a função de extrair, na URL guardo o valor da API
+    na variavel responde eu uso o metodo requests para pegar com get() os valores da api
+    E retorno eles no formato Json'''
+    url = "https://api.coinbase.com/v2/prices/spot"
+    response = requests.get(url)
+    return response.json()
 
-responde = requests.get(url)
+def transformar(dados_json):
+    valor = dados_json['data']['amount']
+    criptomoeda = dados_json['data']['base']
+    moeda = dados_json['data']['currency']
+    dados_tratados = {
+            "valor": valor,
+            "criptomoeda": criptomoeda,
+            "moeda": moeda
+    }
+    return dados_tratados
 
-print(responde.json())
+if __name__ == "__main__":
+    dados_json = extrair()
+    dados_tratados = transformar(dados_json)
+    print(dados_tratados)
