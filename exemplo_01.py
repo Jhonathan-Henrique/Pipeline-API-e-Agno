e81401ea4050835c6792a1ece0bfd74411bce187
Jhonathan Henrique <Jhonathan.Engineer@gmail.com>
2025-05-14 12:55:08 -0300

    chore: add function load and automatized

--- exemplo_01.py
+++ exemplo_01.py
@@ -1,4 +1,8 @@
 import requests
+import time
+from datetime import datetime
+from tinydb import TinyDB
+
 
 def extrair():
     '''Aqui criei a função de extrair, na URL guardo o valor da API
@@ -8,18 +12,28 @@ def extrair():
     response = requests.get(url)
     return response.json()
 
-def transformar(dados_json):
+def transformar(dados_json): #Aqui criei uma função e passei dados_jason como parametro, só isso!
     valor = dados_json['data']['amount']
     criptomoeda = dados_json['data']['base']
     moeda = dados_json['data']['currency']
     dados_tratados = {
             "valor": valor,
             "criptomoeda": criptomoeda,
-            "moeda": moeda
+            "moeda": moeda,
+            "timestamp": datetime.now().isoformat()
     }
     return dados_tratados
 
+def load(dados_tratados):
+    db = TinyDB('db.json')
+    db.insert(dados_tratados)
+    print('Os dados foram salvos com sucesso!')
+
 if __name__ == "__main__":
-    dados_json = extrair()
-    dados_tratados = transformar(dados_json)
-    print(dados_tratados)
+    while True:
+        dados_json = extrair()
+        dados_tratados = transformar(dados_json)
+        load(dados_tratados)
+        time.sleep(5)
+
+
